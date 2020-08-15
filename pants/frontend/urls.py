dae2c32c55d5c9633b2b745f7a5113f4abf31967
Austin Crapo <corey.crapo@gmail.com>
2020-08-15 13:33:25 -0600

    Basic diary manager implementation

--- pants/frontend/urls.py
+++ pants/frontend/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.ingredient_manager, name='ingredient_manager'),
-    url(r'^recipe$', views.recipe_manager, name='recipe_manager')
+    url(r'^recipe$', views.recipe_manager, name='recipe_manager'),
+    url(r'^diary$', views.diary_manager, name='diary_manager'),
 ]
